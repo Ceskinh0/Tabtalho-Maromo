4d41cf261b4965fc7a5e829a90a241ca2a92da6f
Cesk <100101104+Ceskinh0@users.noreply.github.com>
2022-06-05 15:35:29 -0300

    Update main.c

--- projeto_maromo_notaFinal/projeto_maromo/main.c
+++ projeto_maromo_notaFinal/projeto_maromo/main.c
@@ -3,40 +3,46 @@
 #define T 500
 #include "produto.h"
 
-int main()
-{
+int main(){
     int pos = 0;
     int codProc = 0;
     produto* vet_produto[T];
     int opc = 0;
     inicializar(vet_produto, T);
     do{
-        printf("Manutencao de Produto \n");
-        printf("-------------------- \n");
+        printf("=Manutencao de Produtos= \n");
+        printf("----------------------- \n");
         printf("1. Cadastrar Produto \n");
-        printf("2. Procurar Produto \n");
-        printf("3. Atualizar Produto \n");
-        printf("4. Apresentar todos \n");
+        printf("2. Listar Produtos \n");
+        printf("3. Procurar Produto \n");
+        printf("4. Comprar Produtos \n");
+        printf("5. Vender Produtos \n");
         printf("9. Sair \n");
-        printf("Digite sua opcao: \n");
+        printf("Digite sua opcao: ");
         scanf("%d", &opc);
+        system("cls");
         switch(opc){
             case 1:
                 adicionar(vet_produto, pos);
                 pos++;
                 break;
             case 2:
-                printf("Digite o codigo a ser encontrado: \n");
+                listarTodos(vet_produto, pos);
+                break;
+            case 3:
+                printf("Digite o codigo a ser encontrado: ");
                 scanf("%d", &codProc);
                 encontrar(vet_produto, codProc, pos);
                 break;
-            case 3:
-                printf("Digite o codigo a ser atualizado: \n");
-                scanf("%d", &codProc);
-                atualizar(vet_produto, codProc, pos);
-                break;
             case 4:
-                listarTodos(vet_produto, pos);
+                printf("Digite o codigo do produto a ser comprado: ");
+                scanf("%d", &codProc);
+                comprar(vet_produto, codProc, pos);
+                break;
+            case 5:
+                printf("Digite o codigo do produto a ser vendido: ");
+                scanf("%d", &codProc);
+                vender(vet_produto, codProc, pos);
                 break;
             case 9:
                 printf("Fim do programa \n");
