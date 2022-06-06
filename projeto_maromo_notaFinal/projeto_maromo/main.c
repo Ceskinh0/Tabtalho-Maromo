c6cb779bac25f5803a22e1a0355f782f59d3808f
Cesk <100101104+Ceskinh0@users.noreply.github.com>
2022-06-06 12:47:31 -0300

    Update main.c

--- projeto_maromo_notaFinal/projeto_maromo/main.c
+++ projeto_maromo_notaFinal/projeto_maromo/main.c
@@ -10,15 +10,15 @@ int main(){
     int opc = 0;
     inicializar(vet_produto, T);
     do{
-        printf("=Manutencao de Produtos= \n");
-        printf("----------------------- \n");
-        printf("1. Cadastrar Produto \n");
-        printf("2. Listar Produtos \n");
-        printf("3. Procurar Produto \n");
-        printf("4. Comprar Produtos \n");
-        printf("5. Vender Produtos \n");
-        printf("9. Sair \n");
-        printf("Digite sua opcao: ");
+        printf("\t=Manutencao de Produtos= \n");
+        printf("\t----------------------- \n");
+        printf("\t1. Cadastrar Produto \n");
+        printf("\t2. Listar Produtos \n");
+        printf("\t3. Procurar Produto \n");
+        printf("\t4. Comprar Produtos \n");
+        printf("\t5. Vender Produtos \n");
+        printf("\t9. Sair \n");
+        printf("\tDigite sua opcao: ");
         scanf("%d", &opc);
         system("cls");
         switch(opc){
@@ -30,22 +30,22 @@ int main(){
                 listarTodos(vet_produto, pos);
                 break;
             case 3:
-                printf("Digite o codigo a ser encontrado: ");
+                printf("\tDigite o codigo a ser encontrado: ");
                 scanf("%d", &codProc);
                 encontrar(vet_produto, codProc, pos);
                 break;
             case 4:
-                printf("Digite o codigo do produto a ser comprado: ");
+                printf("\tDigite o codigo do produto a ser comprado: ");
                 scanf("%d", &codProc);
                 comprar(vet_produto, codProc, pos);
                 break;
             case 5:
-                printf("Digite o codigo do produto a ser vendido: ");
+                printf("\tDigite o codigo do produto a ser vendido: ");
                 scanf("%d", &codProc);
                 vender(vet_produto, codProc, pos);
                 break;
             case 9:
-                printf("Fim do programa \n");
+                printf("\tFim do programa \n");
                 break;
             default:
                 printf("Opcao invalida \n");
